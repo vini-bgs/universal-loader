0f30f9a2ff605a9fa166b793ff5e0dc9f1353a23
vini-bgs <viniciustheros@gmail.com>
2026-06-28 14:04:55 -0300

    feat: refinando o arquivo run.py

---
+++ runs/run.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+from loguru import logger
+from src.config import FileConfig
+from src.loader import FileLoader
+
+
+FILES_DIR = Path("files/")
+SCHEMA = "dbo"
+
+try:
+    for file in FILES_DIR.iterdir():
+        if file.suffix not in (".csv",):
+            logger.warning(f"⚠️  Extensão '{file.suffix}' não suportada")
+            continue
+
+        config = FileConfig(
+            file_path=str(file), schema_name=SCHEMA, table_name=file.stem
+        )
+
+        df = FileLoader(config).load()
+        print(df.head())
+
+        logger.success("✅ Configuração concluída!")
+
+except FileNotFoundError:
+    logger.error(f"⚠️ Pasta '{FILES_DIR}/' não encontrada")
